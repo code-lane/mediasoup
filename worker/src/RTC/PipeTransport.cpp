0637558d0201f2f1db4bd3a80da342ded417c209
Manuel <manuel@codelane.ch>
2021-01-04 21:22:44 +0100

    do not check tuple for pipe transport

--- worker/src/RTC/PipeTransport.cpp
+++ worker/src/RTC/PipeTransport.cpp
@@ -623,18 +623,18 @@ namespace RTC
 			return;
 		}
 
-		// Verify that the packet's tuple matches our tuple.
-		if (!this->tuple->Compare(tuple))
-		{
-			MS_DEBUG_TAG(rtp, "ignoring RTP packet from unknown IP:port");
+		// // Verify that the packet's tuple matches our tuple.
+		// if (!this->tuple->Compare(tuple))
+		// {
+		// 	MS_DEBUG_TAG(rtp, "ignoring RTP packet from unknown IP:port");
 
-			// Remove this SSRC.
-			RecvStreamClosed(packet->GetSsrc());
+		// 	// Remove this SSRC.
+		// 	RecvStreamClosed(packet->GetSsrc());
 
-			delete packet;
+		// 	delete packet;
 
-			return;
-		}
+		// 	return;
+		// }
 
 		// Pass the packet to the parent transport.
 		RTC::Transport::ReceiveRtpPacket(packet);
@@ -656,13 +656,13 @@ namespace RTC
 			return;
 		}
 
-		// Verify that the packet's tuple matches our tuple.
-		if (!this->tuple->Compare(tuple))
-		{
-			MS_DEBUG_TAG(rtcp, "ignoring RTCP packet from unknown IP:port");
+		// // Verify that the packet's tuple matches our tuple.
+		// if (!this->tuple->Compare(tuple))
+		// {
+		// 	MS_DEBUG_TAG(rtcp, "ignoring RTCP packet from unknown IP:port");
 
-			return;
-		}
+		// 	return;
+		// }
 
 		RTC::RTCP::Packet* packet = RTC::RTCP::Packet::Parse(data, static_cast<size_t>(intLen));
 
@@ -686,12 +686,12 @@ namespace RTC
 			return;
 
 		// Verify that the packet's tuple matches our tuple.
-		if (!this->tuple->Compare(tuple))
-		{
-			MS_DEBUG_TAG(sctp, "ignoring SCTP packet from unknown IP:port");
+		// if (!this->tuple->Compare(tuple))
+		// {
+		// 	MS_DEBUG_TAG(sctp, "ignoring SCTP packet from unknown IP:port");
 
-			return;
-		}
+		// 	return;
+		// }
 
 		// Pass it to the parent transport.
 		RTC::Transport::ReceiveSctpData(data, len);
